d8e1f24e4f77a8c20d4ee4360015c8627f76547e
salaheddine <salaheddine.zizaan@gmail.com>
2026-08-29 21:24:58 +0100

    onboarding interface

---
+++ src/constants/images.ts
@@ -0,0 +1,5 @@
+export const images = {
+  onboardingWelcomeWomen: require("../../assets/images/onboarding/welcome-women.png"),
+  weraLogoWithName: require("../../assets/logos/wera-logo-with-name.png"),
+  onboardingWelcomeMen: require("../../assets/images/onboarding/welcome-men.png"),
+} as const;
